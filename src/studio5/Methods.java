b62e6c4195ce4a8d4f775dbda69f8032825ba9b0
jiarun <jiarun@jiaruns-air.dhcp.wustl.edu>
2022-10-20 11:48:51 -0500

    1

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -14,8 +14,11 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
+		
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
+		double distance = Math.sqrt(Math.pow((x2-x1),(2))+Math.pow((y2-y1),(2)));
+		
+		
 		
 		return distance;
 	}
